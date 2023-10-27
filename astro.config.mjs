cd2ade3d9d5b0204761818e915efdd9445aa5ae5
Rian Hsu <hey@rian.cc>
2023-10-27 13:41:23 +0800

    feat: netlify cms change ro decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import rehypeSlug from "rehype-slug";
-import NetlifyCMS from "astro-netlify-cms";
+// import NetlifyCMS from "astro-netlify-cms";
 import astroI18next from "astro-i18next";
 import alpinejs from "@astrojs/alpinejs";
 import AstroPWA from "@vite-pwa/astro";
@@ -21,109 +21,109 @@ export default defineConfig({
 	integrations: [
 		tailwind(),
 		sitemap(),
-		NetlifyCMS({
-			config: {
-				backend: {
-					name: "github",
-					repo: "heyrian/riancc",
-					branch: "main",
-					base_url: config.site.base_url,
-					auth_endpoint: "/api/auth",
-				},
-				media_folder: "public/images",
-				public_folder: "/images",
-				i18n: {
-					structure: "multiple_folders",
-					locales: ["zh","en", "ja"],
-					default_locale: "zh",
-				},
-				collections: [
-					// Content collections
-					{
-						name: "posts",
-						i18n: true,
-						label: "Blog Posts",
-						folder: "src/content/blog",
-						slug:"{{customSlug}}",
-						create: true,
-						delete: true,
-						fields: [
-							{
-								name: "title",
-								widget: "string",
-								label: "Post Title",
-								i18n: true,
-							},
-							{
-								name: "customSlug",
-								widget: "string",
-								label: "FileName",
-								i18n: "duplicate"
-							},
-							{
-								label: "Draft",
-								name: "draft",
-								widget: "boolean",
-								i18n: "duplicate",
-							},
-							{
-								label: "Tags",
-								name: "tags",
-								widget: "list",
-								i18n: true,
-							},
-							{
-								label: "Image",
-								name: "image",
-								widget: "object",
-								i18n: true,
-								fields: [
-									{
-										label: "Source",
-										name: "src",
-										widget: "image",
-										i18n: "duplicate",
-									},
-									{
-										label: "Alt Text",
-										name: "alt",
-										widget: "string",
-										i18n: true,
-									},
-								],
-							},
-							{
-								label: "Snippet",
-								name: "snippet",
-								widget: "text",
-								i18n: true,
-							},
-							{
-								label: "Publish Date",
-								name: "publishDate",
-								widget: "datetime",
-								format: "YYYY-MM-DD HH:mm",
-								i18n: "duplicate",
-							},
-							{
-								label: "Category",
-								name: "category",
-								widget: "select",
-								options: ["Development", "Business", "Design", "Life"],
-								i18n: "duplicate",
-							},
-							{
-								name: "body",
-								widget: "markdown",
-								label: "Post Body",
-								i18n: true,
-							},
-						],
-					},
-				],
-			},
-			disableIdentityWidgetInjection: true,
-		}),
+		// NetlifyCMS({
+		// 	config: {
+		// 		backend: {
+		// 			name: "github",
+		// 			repo: "heyrian/riancc",
+		// 			branch: "main",
+		// 			base_url: config.site.base_url,
+		// 			auth_endpoint: "/api/auth",
+		// 		},
+		// 		media_folder: "public/images",
+		// 		public_folder: "/images",
+		// 		i18n: {
+		// 			structure: "multiple_folders",
+		// 			locales: ["zh","en", "ja"],
+		// 			default_locale: "zh",
+		// 		},
+		// 		collections: [
+		// 			// Content collections
+		// 			{
+		// 				name: "posts",
+		// 				i18n: true,
+		// 				label: "Blog Posts",
+		// 				folder: "src/content/blog",
+		// 				slug:"{{customSlug}}",
+		// 				create: true,
+		// 				delete: true,
+		// 				fields: [
+		// 					{
+		// 						name: "title",
+		// 						widget: "string",
+		// 						label: "Post Title",
+		// 						i18n: true,
+		// 					},
+		// 					{
+		// 						name: "customSlug",
+		// 						widget: "string",
+		// 						label: "FileName",
+		// 						i18n: "duplicate"
+		// 					},
+		// 					{
+		// 						label: "Draft",
+		// 						name: "draft",
+		// 						widget: "boolean",
+		// 						i18n: "duplicate",
+		// 					},
+		// 					{
+		// 						label: "Tags",
+		// 						name: "tags",
+		// 						widget: "list",
+		// 						i18n: true,
+		// 					},
+		// 					{
+		// 						label: "Image",
+		// 						name: "image",
+		// 						widget: "object",
+		// 						i18n: true,
+		// 						fields: [
+		// 							{
+		// 								label: "Source",
+		// 								name: "src",
+		// 								widget: "image",
+		// 								i18n: "duplicate",
+		// 							},
+		// 							{
+		// 								label: "Alt Text",
+		// 								name: "alt",
+		// 								widget: "string",
+		// 								i18n: true,
+		// 							},
+		// 						],
+		// 					},
+		// 					{
+		// 						label: "Snippet",
+		// 						name: "snippet",
+		// 						widget: "text",
+		// 						i18n: true,
+		// 					},
+		// 					{
+		// 						label: "Publish Date",
+		// 						name: "publishDate",
+		// 						widget: "datetime",
+		// 						format: "YYYY-MM-DD HH:mm",
+		// 						i18n: "duplicate",
+		// 					},
+		// 					{
+		// 						label: "Category",
+		// 						name: "category",
+		// 						widget: "select",
+		// 						options: ["Development", "Business", "Design", "Life"],
+		// 						i18n: "duplicate",
+		// 					},
+		// 					{
+		// 						name: "body",
+		// 						widget: "markdown",
+		// 						label: "Post Body",
+		// 						i18n: true,
+		// 					},
+		// 				],
+		// 			},
+		// 		],
+		// 	},
+		// 	disableIdentityWidgetInjection: true,
+		// }),
 		astroI18next(),
 		alpinejs(),
 		AstroPWA({
